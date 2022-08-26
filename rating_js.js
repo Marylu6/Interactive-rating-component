ad33c23a60311568868ce20f69f83559bff9bf8c
Ashley <62220733+Marylu6@users.noreply.github.com>
2022-08-26 13:27:23 -0400

    Add files via upload

---
+++ rating_js.js
@@ -0,0 +1,21 @@
+const submitContainer = document.getElementById('Selected-Rating');
+const btnSelector = document.getElementsByClassName('btn');
+
+function submitRating() {
+    for (let i = 0; i < btnSelector.length; i += 1) {
+        btnSelector[i].addEventListener('click', () => {
+            submitContainer.innerHTML = `You Selected ${i += 1} out of 5`;
+        });
+    }
+}
+
+document.getElementById('submit').addEventListener('click', () => {
+    if (submitContainer.childNodes.length === 0) {
+        document.getElementById('display-error').style.display = 'block';
+    } else {
+        document.getElementById('Select-container').style.display = 'none';
+        document.getElementById('Submit-Window').style.display = 'flex';
+    }
+});
+
+submitRating();
